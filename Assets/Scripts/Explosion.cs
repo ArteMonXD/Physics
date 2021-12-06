ba2030bc6626d42243e2e0ea90d940531c4ff165
ArteMonXD <70392374+ArteMonXD@users.noreply.github.com>
2021-12-06 04:39:10 +0300

    Выполнил челендж с взрывом

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    public float powerExplosion;
+    public float radius;
+    private Rigidbody[] explosiveRb;
+    void Start()
+    {
+        explosiveRb = FindObjectsOfType<Rigidbody>();
+    }
+    
+    public void Boom()
+    {
+        foreach(Rigidbody rb in explosiveRb)
+        {
+            float distance = Vector3.Distance(transform.position, rb.transform.position);
+            if(distance <= radius)
+            {
+                Vector3 direction = rb.transform.position - transform.position;
+                rb.AddForce(direction.normalized * (powerExplosion * (radius - distance)), ForceMode.Impulse);
+            }
+        }
+    }
+}
