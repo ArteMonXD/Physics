d16119e7d2954cb26534d60fc6d27c58a9064954
ArteMonXD <70392374+ArteMonXD@users.noreply.github.com>
2021-12-10 02:36:39 +0300

    Добавлено управление SuperMan

--- Assets/Scripts/SuperMan.cs
+++ Assets/Scripts/SuperMan.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class SuperMan : MonoBehaviour
 {
     public float pushPower;
+    public float sensitivityHor = 9.0f;
+    public float speedMove = 10.0f;
+    private void Update()
+    {
+        RotateControll();
+        MoveControll();
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.GetComponent<Rigidbody>() != null)
@@ -12,6 +19,18 @@ public class SuperMan : MonoBehaviour
             Rigidbody badGuyRB = collision.gameObject.GetComponent<Rigidbody>();
             Vector3 direction = collision.transform.position - transform.position;
             badGuyRB.AddForce(direction * pushPower, ForceMode.Impulse);
+        }
+    }
+    private void RotateControll()
+    {
+        transform.Rotate(0, (Input.GetAxis("Mouse X") * sensitivityHor) * Time.deltaTime, 0);
+    }
+    private void MoveControll()
+    {
+        if(Input.GetAxis("Vertical") > 0)
+        {
+            float deltaZ = Input.GetAxis("Vertical") * speedMove;
+            transform.Translate(0, 0, deltaZ * Time.deltaTime);
         }
     }
 }
